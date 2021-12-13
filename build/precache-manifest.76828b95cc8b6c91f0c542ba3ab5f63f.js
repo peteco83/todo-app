28b4a8a85b0fc66c2b875d14e8ec235dac8b8b15
peteco83 <peteco1983@outlook.com>
2021-12-13 19:43:22 +0100

    added build

---
+++ build/precache-manifest.76828b95cc8b6c91f0c542ba3ab5f63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d94bd13301875133c43e6fe103b511c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06a802e3587166be9f06",
+    "url": "/static/css/main.b1657efd.chunk.css"
+  },
+  {
+    "revision": "507cf5133a360aa99490",
+    "url": "/static/js/2.73091361.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.73091361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a802e3587166be9f06",
+    "url": "/static/js/main.ded748a5.chunk.js"
+  },
+  {
+    "revision": "7e6d8c24c5d3b82fe6be",
+    "url": "/static/js/runtime-main.6f88f9a7.js"
+  }
+]);
